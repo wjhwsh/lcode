a050c97d354bab50f6c4804ad5dd9ac9c671205c
Jianhua Wang <wjhwsh@gmail.com>
2021-03-29 15:37:43 -0700

    add 929

---
+++ 929.unique-email-addresses.472459969.ac.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        seen = set()
+        for email in emails:
+            local, domain = email.split('@')
+            if '+' in local:
+                local = local[:local.index('+')]
+            
+            seen.add(local.replace('.', '') +'@' +domain)
+        return len(seen)
